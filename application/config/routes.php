62f3750ea01aa8e646759e3f908380f4024e2ea2
Eric Niemeyer <49417819+niembro64@users.noreply.github.com>
2022-11-28 17:18:46 -0500

    form working but not redirecting correctly

--- application/config/routes.php
+++ application/config/routes.php
@@ -53,6 +53,12 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 // $route['404_override'] = '';
 // $route['translate_uri_dashes'] = FALSE;
 
+// $route['news/(:any)'] = 'news/view/$1';
+// $route['news'] = 'news';
+// $route['(:any)'] = 'pages/view/$1';
+// $route['default_controller'] = 'pages/view';
+
+$route['news/create'] = 'news/create';
 $route['news/(:any)'] = 'news/view/$1';
 $route['news'] = 'news';
 $route['(:any)'] = 'pages/view/$1';
