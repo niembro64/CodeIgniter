77ceb7eebaddca48b99ab5a1a7a73dd0273338de
Eric Niemeyer <eric.niemeyer@venturetec.net>
2022-11-30 14:12:18 -0500

    done

--- application/views/news/index.php
+++ application/views/news/index.php
@@ -5,6 +5,7 @@
   <?php echo $news_item['text']; ?>
   <div class="main">
   </div>
-  <p><a href="<?php echo site_url('news/' . $news_item['slug']); ?>">View <?php echo $news_item['id'] ?></a></p>
+  <a href="<?php echo site_url('news/' . $news_item['slug']); ?>"><button>View <?php echo $news_item['id'] ?></button></a>
   <a href="<?php echo site_url('news/delete/' . $news_item['id']); ?>"><button>Delete <?php echo $news_item['id'] ?></button></a>
+  <a href="<?php echo site_url('news/update/' . $news_item['slug']); ?>"><button>Update <?php echo $news_item['id'] ?></button></a>
 <?php endforeach; ?>
