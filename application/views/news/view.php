2bd7b8e324da59e8689c714cae7131ac956018eb
Eric Niemeyer <eric.niemeyer@venturetec.net>
2022-11-30 15:17:02 -0500

    pre showing dave

--- application/views/news/view.php
+++ application/views/news/view.php
@@ -1,4 +1,6 @@
-<?php
-echo '<h2>' . $news_item['title'] . '</h2>';
-echo '<p>' . $news_item['text'] . '</p>';
-// echo '<p><a href="' . site_url('news/') . '">Back</a></p>';
+<div class="article-preview">
+  <?php
+  echo '<h2>' . $news_item['title'] . '</h2>';
+  echo '<p>' . $news_item['text'] . '</p>';
+  ?>
+</div>
--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,6 +1,7 @@
+<div class="footer">
+  <em>&copy; <?php echo date("Y"); ?> niembro64</em>
+</div>
 
-<em>&copy; <?php echo date("Y"); ?> niembro64</em>
-<!-- <em>&copy; 2015</em> -->
 </body>
 
 </html>
