b1dfbad75d0250238c2cd91d13ceb77251222518
Eric Niemeyer <eric.niemeyer@venturetec.net>
2022-11-30 14:17:18 -0500

    added some br

--- application/views/news/index.php
+++ application/views/news/index.php
@@ -4,8 +4,13 @@
 
   <h3><?php echo $news_item['id'] . " " . $news_item['title']; ?></h3>
   <?php echo $news_item['text']; ?>
+  <br>
+  <br>
+  
   <a href="<?php echo site_url('news/' . $news_item['slug']); ?>"><button>View <?php echo $news_item['id'] ?></button></a>
   <a href="<?php echo site_url('news/delete/' . $news_item['id']); ?>"><button>Delete <?php echo $news_item['id'] ?></button></a>
   <a href="<?php echo site_url('news/update/' . $news_item['slug']); ?>"><button>Update <?php echo $news_item['id'] ?></button></a>
-
+  <br>
+  <br>
+  
 <?php endforeach; ?>
