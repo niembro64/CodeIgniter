f34d0dabb222609aa902af84c8fdc2e0c0351f2f
Eric Niemeyer <eric.niemeyer@venturetec.net>
2022-11-30 13:52:57 -0500

    changed username

--- application/views/news/create.php
+++ application/views/news/create.php
@@ -1,8 +1,8 @@
 <?php echo validation_errors(); ?>
 <?php echo form_open('news/create'); ?>
-<label for="title">Article Title:</label>
+<label for="title">Title:</label>
 <input type="text" name="title" /><br />
-<label for="text">Article Text:</label>
+<label for="text">Text:</label>
 <textarea name="text"></textarea><br />
 <input type="submit" name="submit" value="Add" />
 </form>
