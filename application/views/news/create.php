d337e7efaf49d373ec82150609bf45ac5dbf0b33
Eric Niemeyer <49417819+niembro64@users.noreply.github.com>
2022-11-30 13:49:29 -0500

    clean up

--- application/views/news/create.php
+++ application/views/news/create.php
@@ -1,8 +1,8 @@
 <?php echo validation_errors(); ?>
 <?php echo form_open('news/create'); ?>
-<label for="title">News Title:</label>
+<label for="title">Article Title:</label>
 <input type="text" name="title" /><br />
-<label for="text">What Happened:</label>
+<label for="text">Article Text:</label>
 <textarea name="text"></textarea><br />
 <input type="submit" name="submit" value="Add" />
 </form>
