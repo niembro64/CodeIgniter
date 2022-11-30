fc872b71a8dfe3905322366ebcf9c71447ef0779
Eric Niemeyer <eric.niemeyer@venturetec.net>
2022-11-30 14:14:45 -0500

    delete unnecessary thing

--- application/views/news/index.php
+++ application/views/news/index.php
@@ -1,11 +1,11 @@
 <!-- <h2><?php echo $title; ?></h2> -->
 
 <?php foreach ($news as $news_item) : ?>
+
   <h3><?php echo $news_item['id'] . " " . $news_item['title']; ?></h3>
   <?php echo $news_item['text']; ?>
-  <div class="main">
-  </div>
   <a href="<?php echo site_url('news/' . $news_item['slug']); ?>"><button>View <?php echo $news_item['id'] ?></button></a>
   <a href="<?php echo site_url('news/delete/' . $news_item['id']); ?>"><button>Delete <?php echo $news_item['id'] ?></button></a>
   <a href="<?php echo site_url('news/update/' . $news_item['slug']); ?>"><button>Update <?php echo $news_item['id'] ?></button></a>
+
 <?php endforeach; ?>
